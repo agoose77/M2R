0917f90c7c1baf829cd4257f7aa39db2d396e86f
psantarita <pedrosarial@gmail.com>
2018-02-08 15:33:10 +0000

    Leafs for detectors

--- cheatsheetC.C
+++ cheatsheetC.C
@@ -9,3 +9,28 @@ for (size_t t = 0; t < 60; t++) {
  cout<< unsignedchar(data[t]) << "\n";
  data->Branch("Event",&Event,"Event[%t]/I");
 }
+
+//medio funcionando
+
+TFile f("data.root","UPDATE"); //ROOT file
+TTree *data = new TTree("data","Tree data"); //Tree to store the Data
+data->Write("data.root",TObject::kOverwrite); // Making the root file
+//buenazo mi carnal
+char tree_[20];//
+for (int j=0;j<200;j++){
+//sprintf(tree_,"DET%i[%i]",j);
+data.Branch(TString::Format("DET%i",j), &tree_, TString::Format("DET%i[%i]/I", j)); //bueno
+}
+TBrowser n
+
+
+///prueba buenaza
+TFile f("data.root","UPDATE"); //ROOT file
+TTree *data = new TTree("data","Tree data"); //Tree to store the Data
+data->Write("data.root",TObject::kOverwrite); // Making the root file
+//buenazo mi carnal
+Int_t DET[200]
+for (int j=0;j<200;j++){
+data.Branch(TString::Format("DET[%i]", j), &DET, TString::Format("DET[%i]/I", j)); //bueno
+}
+TBrowser n
