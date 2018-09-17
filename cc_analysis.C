0b287c2720b34d8f1a7ba0358b2b01eec389f09f
psantarita <pedrosarial@gmail.com>
2018-09-17 13:58:27 +0100

    Thanos solution

--- cc_analysis.C
+++
@@ -1,4 +0,0 @@
-//PEAK FINDER
-
-TSpectrum *s = new TSpectrum(3);
-Int_t nfound = s->Search(D9,1,"new");
--- histo2ascci.C
+++
@@ -1,9 +0,0 @@
-void histo2ascci (TH1* h)
-{
-   Int_t n = h->GetNbinsX();
-   for (Int_t i=1; i<=n; i++) {
-      printf("%g, %g\n",
-             int(h->GetBinLowEdge(i)+h->GetBinWidth(i)/2),
-             h->GetBinContent(i));
-   }
-}
